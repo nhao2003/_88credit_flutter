9b3e9d3cab55e775364caf1bb5f7d9abaf282ad6
minhphan46 <21522345@gm.uit.edu.vn>
2023-12-11 13:35:39 +0700

    change avatar

--- lib/features/presentation/modules/contract/contract_controler.dart
+++ lib/features/presentation/modules/contract/contract_controler.dart
@@ -71,7 +71,7 @@ class ContractController extends GetxController {
           "Our loan products can be used for a variety of reasons, from covering unexpected expenses to financing major purchases. Whether you're looking to consolidate high-interest debt, make home improvements, pay for medical expenses, or cover educational costs, we offer loans with flexible repayment terms and competitive interest rates. Our loan officers are available to work with you to understand your specific needs and help you select the best loan option for your situation. Contact us today to learn more about our loan products and how we can help you achieve your financial goals.",
       videoConfirmation: "videoConfirmation",
       portraitPhoto:
-          "https://faceinch.vn/upload/news/chup-anh-the-tha-toc-3007.jpg",
+          "https://img.freepik.com/premium-photo/anime-male-avatar_950633-956.jpg",
       idCardFrontPhoto:
           "https://congan.tiengiang.gov.vn/documents/2261410/29734928/image005.jpg/a25a6795-e346-4d5c-9fc8-ad977f68c258?t=1615901441563",
       idCardBackPhoto:
