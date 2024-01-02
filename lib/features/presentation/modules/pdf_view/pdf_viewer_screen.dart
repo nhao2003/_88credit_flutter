421e2916deb04f7c37206f3fb593e292bfe97505
minhphan46 <21522345@gm.uit.edu.vn>
2024-01-02 14:29:31 +0700

    contract pdf

--- lib/features/presentation/modules/pdf_view/pdf_viewer_screen.dart
+++ lib/features/presentation/modules/pdf_view/pdf_viewer_screen.dart
@@ -27,39 +27,40 @@ class PdfViewerScreen extends StatelessWidget {
     content = content.replaceAll('{lender_name}', contract.lender!.fullName);
     content = content.replaceAll('{lender_dob}', contract.lender!.dob!);
     content = content.replaceAll(
-        '{lender_id_card_number}', contract.lenderBankCardId!);
+        '{lender_id_card_number}', contract.lenderBankCard!.cardNumber!);
     content =
-        content.replaceAll('{lender_id_card_issued_by}', 'Local Authority');
+        content.replaceAll('{lender_id_card_issued_by}', 'Ủy ban nhân dân');
     content = content.replaceAll('{lender_id_card_issued_at}', '01/01/2020');
     content = content.replaceAll('{lender_permanent_residence}',
         contract.lender!.address!.getDetailAddress());
     content =
         content.replaceAll('{lender_phone_number}', contract.lender!.phone!);
-    content =
-        content.replaceAll('{lender_bank_name}', contract.lenderBankCardId!);
     content = content.replaceAll(
-        '{lender_bank_account_number}', contract.lenderBankCardId!);
-    content =
-        content.replaceAll('{lender_bank_branch}', contract.lenderBankCardId!);
+        '{lender_bank_name}', contract.lenderBankCard!.bank!.name);
+    content = content.replaceAll(
+        '{lender_bank_account_number}', contract.lenderBankCard!.cardNumber!);
+    content = content.replaceAll('{lender_bank_branch}',
+        contract.lenderBankCard!.branch ?? "Hồ Chí Minh");
     content =
         content.replaceAll('{borrower_name}', contract.borrower!.fullName);
     content = content.replaceAll('{borrower_dob}', contract.borrower!.dob!);
     content = content.replaceAll(
-        '{borrower_id_card_number}', contract.borrowerBankCardId!);
+        '{borrower_id_card_number}', contract.borrowerBankCard!.cardNumber!);
     content =
-        content.replaceAll('{borrower_id_card_issued_by}', 'Local Authority');
+        content.replaceAll('{borrower_id_card_issued_by}', 'Ủy ban nhân dân');
     content = content.replaceAll('{borrower_id_card_issued_at}', '01/01/2020');
     content = content.replaceAll('{borrower_permanent_residence}',
         contract.borrower!.address!.getDetailAddress());
     content = content.replaceAll(
         '{borrower_phone_number}', contract.borrower!.phone!);
     content = content.replaceAll(
-        '{borrower_bank_name}', contract.borrowerBankCardId!);
-    content = content.replaceAll(
-        '{borrower_bank_account_number}', contract.borrowerBankCardId!);
-    content = content.replaceAll(
-        '{borrower_bank_branch}', contract.borrowerBankCardId!);
-    content = content.replaceAll('{amount}', contract.amount!.toString());
+        '{borrower_bank_name}', contract.borrowerBankCard!.bank!.name);
+    content = content.replaceAll('{borrower_bank_account_number}',
+        contract.borrowerBankCard!.cardNumber!);
+    content = content.replaceAll('{borrower_bank_branch}',
+        contract.borrowerBankCard!.branch ?? "Hồ Chí Minh");
+    content =
+        content.replaceAll('{amount}', contract.amount!.toInt().toString());
     content = content.replaceAll(
         '{amount_in_words}', contract.amount!.toInt().convertNumberToWords());
     content = content.replaceAll('{loan_reason}', contract.loanReason!);
